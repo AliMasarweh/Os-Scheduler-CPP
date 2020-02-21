698e7fa78fa45bd910cabe213d34734825a2a2f8
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-21 10:29:51 -0800

    Scheduler API

--- scheduler.h
+++ scheduler.h
@@ -5,9 +5,20 @@
 #ifndef OS_SCHEDULER_SCHEDULER_H
 #define OS_SCHEDULER_SCHEDULER_H
 
+#include <vector>
+
+class Task;
+
 class Scheduler {
 public:
     static unsigned long currentTime();
+    static void addTask(Task& t);
+    static Time runAllTasks();
+
+private:
+    static unsigned long s_time;
+    /* This a collections indexes tasks by their priority */
+    static std::vector<std::vector<Task*> > s_tasksByPriority;
 };
 
 #endif //OS_SCHEDULER_SCHEDULER_H
