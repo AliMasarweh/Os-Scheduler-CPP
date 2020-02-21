21eeadff9cfc42969055115d7f2b01be14622c61
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-21 06:57:15 -0800

    API of Time class

--- time.h
+++ time.h
@@ -4,5 +4,29 @@
 
 #ifndef OS_SCHEDULER_TIME_H
 #define OS_SCHEDULER_TIME_H
-class Time {};
+class Time {
+
+    friend Time operator+(const Time& time1, const Time& time2);
+    friend Time operator-(const Time& time1, const Time& time2);
+
+public:
+    Time(unsigned long t = 0);
+
+    unsigned long getTime() const ;
+
+    void now() const;
+
+    Time& operator+=(const Time& t);
+    Time& operator-=(const Time& t);
+
+    bool operator<(const Time& t) const ;
+    bool operator>(const Time& t) const ;
+    bool operator==(const Time& t) const ;
+    bool operator!=(const Time& t) const ;
+    bool operator<=(const Time& t) const ;
+    bool operator>=(const Time& t) const ;
+
+private:
+    unsigned long time;
+};
 #endif //OS_SCHEDULER_TIME_H
