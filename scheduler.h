4f8d1ba6bbe697ab23d54c36a3652940c7a61186
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-21 10:31:49 -0800

    More approproaite use for time class

--- scheduler.h
+++ scheduler.h
@@ -6,17 +6,18 @@
 #define OS_SCHEDULER_SCHEDULER_H
 
 #include <vector>
+#include "time.h"
 
 class Task;
 
 class Scheduler {
 public:
-    static unsigned long currentTime();
+    static Time currentTime();
     static void addTask(Task& t);
     static Time runAllTasks();
 
 private:
-    static unsigned long s_time;
+    static Time s_time;
     /* This a collections indexes tasks by their priority */
     static std::vector<std::vector<Task*> > s_tasksByPriority;
 };
