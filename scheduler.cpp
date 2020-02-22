3bc5cf1608c8a6e8ebbb7d823ea9712d1571d17e
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-22 06:46:18 -0800

    Implemented the scheduler using priorities strategy

--- scheduler.cpp
+++ scheduler.cpp
@@ -2,9 +2,12 @@
 // Created by ali-masa on 2/21/20.
 //
 
+#include <time.h>
 #include "scheduler.h"
 
-std::vector<std::vector<Task*> >
+using namespace std;
+
+vector<vector<Task*> >
         Scheduler::s_tasksByPriority = Scheduler::initTasksContainer();
 
 const Time& Scheduler::currentTime() {
@@ -13,22 +16,36 @@ const Time& Scheduler::currentTime() {
 
 void Scheduler::addTask(Task &t)
 {
-    unsigned char priority = t.getPriority();
-    s_tasksByPriority[priority].push_back(&t);
+    unsigned char taskPriority = t.getPriority();
+    s_tasksByPriority[taskPriority].push_back(&t);
 }
 
 Time Scheduler::runAllTasks() {
     Time totalRunningTime(0);
+    int clockPerSecToMilliSec = 1000;
+
+    for(unsigned char i = s_highestPriority-1; i >= 0; --i)
+    {
+        vector<Task *> tasks = s_tasksByPriority[i];
+        for(size_t j = 0; j < tasks.size(); ++j)
+        {
+            Task * task = tasks.at(i);
+            clock_t time = clock();
+            task->run();
+            totalRunningTime += (clock() - time)/
+                    CLOCKS_PER_SEC*clockPerSecToMilliSec;
+        }
+    }
 
     return totalRunningTime;
 }
 
-std::vector<std::vector<Task *> > Scheduler::initTasksContainer() {
-    std::vector<std::vector<Task *> > ans(s_highestPriority);
+vector<vector<Task *> > Scheduler::initTasksContainer() {
+    vector<vector<Task *> > ans(s_highestPriority);
 
     for(unsigned char i = 0; i < s_highestPriority; ++i)
     {
-        ans.push_back(std::vector<Task *>());
+        ans.push_back(vector<Task *>());
     }
 
     return ans;
