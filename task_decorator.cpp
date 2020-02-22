a5277e4d0733bf03403ada9087c25e4ba39d8da7
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-22 06:55:48 -0800

    Implemented a task decorator to add functionality to a basic task

---
+++ task_decorator.cpp
@@ -0,0 +1,22 @@
+//
+// Created by ali-masa on 2/22/20.
+//
+
+#include "task_decorator.h"
+
+TaskDecorator::TaskDecorator(Task &t) {
+    this->task = &t;
+}
+
+void TaskDecorator::run() {
+    this->task->run();
+}
+
+unsigned long TaskDecorator::getNextRunPeriod() {
+    return this->task->getPriority();
+}
+
+unsigned char TaskDecorator::getPriority() {
+    return this->task->getPriority();
+}
+
---
+++ task_decorator.h
@@ -0,0 +1,20 @@
+//
+// Created by ali-masa on 2/22/20.
+//
+
+#ifndef OS_SCHEDULER_TASK_DECORATOR_H
+#define OS_SCHEDULER_TASK_DECORATOR_H
+
+#include "task.h"
+
+class TaskDecorator: public Task
+{
+    Task* task;
+public:
+    TaskDecorator(Task& t);
+    virtual void run();
+    virtual unsigned long getNextRunPeriod();
+    virtual unsigned char getPriority();
+};
+
+#endif //OS_SCHEDULER_TASK_DECORATOR_H
