bb8cc1300cd00da045c458b106825cec894cec2e
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-22 08:23:26 -0800

    Implemented a BasicTask

---
+++ basic_task.cpp
@@ -0,0 +1,26 @@
+//
+// Created by ali-masa on 2/22/20.
+//
+
+#include "basic_task.h"
+#include "scheduler.h"
+
+void BasicTask::run() {
+    // Open the stack
+}
+
+unsigned long BasicTask::getNextRunPeriod() {
+    unsigned long time = Scheduler::currentTime().getTime();
+    if(m_executionTime > time)
+        return (m_executionTime - time).getTime();
+
+    return 0;
+}
+
+unsigned char BasicTask::getPriority() {
+    return this->m_priority;
+}
+
+BasicTask::BasicTask(unsigned char priority, unsigned long waitingTime)
+:m_priority(priority),
+m_executionTime(Scheduler::currentTime() + waitingTime) {}
---
+++ basic_task.h
@@ -0,0 +1,25 @@
+//
+// Created by ali-masa on 2/22/20.
+//
+
+#ifndef OS_SCHEDULER_BASIC_TASK_H
+#define OS_SCHEDULER_BASIC_TASK_H
+
+#include "task.h"
+#include "time.h"
+
+class BasicTask: public Task {
+public:
+    BasicTask(unsigned char priority = 1, unsigned long waitingTime = 0);
+    virtual void run();
+    virtual unsigned long getNextRunPeriod();
+    virtual unsigned char getPriority();
+
+private:
+    Time m_runningTime;
+    unsigned char m_priority;
+    Time m_executionTime;
+};
+
+
+#endif //OS_SCHEDULER_BASIC_TASK_H
