1312505099c58a18556794ae82e801e59ee70229
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-23 11:55:42 -0800

    Changed schudeler API

--- scheduler.h
+++ scheduler.h
@@ -6,6 +6,7 @@
 #define OS_SCHEDULER_SCHEDULER_H
 
 #include <vector>
+#include <queue>
 #include "time.h"
 
 class Task;
@@ -14,15 +15,13 @@ class Scheduler {
 public:
     static const Time& currentTime();
     static void addTask(Task& t);
+    static bool removeTask(Task& t);
     static Time runAllTasks();
 
 private:
     static std::vector<std::vector<Task *> > initTasksContainer();
-
+    static std::priority_queue<Time, std::vector<Task*>, std::greater<Time> > s_prioritizedTasks;
     static Time s_time;
-    /* This a collections indexes tasks by their priority */
-    const static unsigned char s_highestPriority = 10;
-    static std::vector<std::vector<Task*> > s_tasksByPriority;
     static size_t s_numberOfTasks;
 };
 
